a73c36451d042faef62d6980195cc255241dd230
MrN7King <mrn7king@gmail.com>
2025-06-24 23:44:13 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+const App = () => {
+  return (
+    <div className='container mx-auto max-w-7xl'>
+      {/*Navbar*/}
+      {/*Hero*/}
+      {/*Best Sellers Cards*/}
+      {/*About Us*/}
+      {/*Premium Account Link*/}
+      {/*Premium Account Cards*/}
+      {/*Footer*/}
+    </div>
+  )
+}
+
+export default App
