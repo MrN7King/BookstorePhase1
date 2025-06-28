023241f6484d804612f5f45cbd8174d0d439d2e0
Dilaksan2005 <dilaksangnanasegaran@gmail.com>
2025-06-28 13:34:39 +0530

    React-router-dom-v6+ has been intergrated

---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import BestSellerSlider from "@/sections/BestSellerSlider";
+import { FooterWithSitemap } from "@/sections/Footer";
+import HeroSectionOne from "@/sections/HeroSectionOne";
+import Info_Homepage from '@/sections/Info_Homepage';
+import Navigation from "@/sections/Navigation";
+import PremAccLink from "@/sections/PremAccLink";
+import PremiumSlider from "@/sections/PremiumSlider";
+
+const Home = () => {
+  return (
+    <>
+    <div className='container mx-auto pt-16 overflow-hidden'>
+     <Navigation /></div>
+     <HeroSectionOne />
+     <BestSellerSlider />
+     <hr></hr>
+     <div className='w-full'> <Info_Homepage /> </div>
+     <hr></hr>
+     <div className="pt-20">  <PremAccLink/> </div>
+                              <PremiumSlider/>
+     <div className="pt-20">  <FooterWithSitemap /></div>
+    </>
+  )
+}
+
+export default Home
